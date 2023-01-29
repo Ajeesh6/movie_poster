03722582437d599c88d8d8d303a7fe8d9c12ae43
Ajeesh V S <ajeeshvs185@gmail.com>
2023-01-29 22:46:51 +0530

    initial

---
+++ movie.js
@@ -0,0 +1,17 @@
+function search(){
+    destination=dest.value
+    fetch(`https://imdb-api.com/en/API/SearchMovie/k_6dm3gxws/${destination}`).then(data=>data.json())
+    .then(data=>dispayData(data))
+
+}
+
+function dispayData(destinationDetails){
+    Destination=destinationDetails.results[0].title
+    weather=destinationDetails.results[0].image
+    description=destinationDetails.results[0].description
+   
+    result.innerHTML=`<p style="font-size:20px;color:white"><span style=" font-family: inherit;font-weight: bold;font-size: 35px;">${Destination}</span>
+    <span>-${description}</span></P>
+    <img src="${weather}" class="card-img-top" alt="...">`
+   
+}
